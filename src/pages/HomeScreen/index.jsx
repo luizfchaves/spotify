7bba5546a28c01654c9130a7dc512fd6dee552c8
Luiz <lf1405@hotmail.com>
2020-05-19 18:50:02 -0300

    Removendo modal incial

--- src/pages/HomeScreen/index.jsx
+++ src/pages/HomeScreen/index.jsx
@@ -7,21 +7,6 @@ export default function Homescreen() {
   let [alertModal, setAlertModal] = useState(true);
   return (
     <>
-      <Modal isOpen={alertModal} id="alert-modal">
-        <ModalBody>
-          O projeto feito para o desafio trinks mudou de URL.
-        </ModalBody>
-        <ModalFooter>
-          <Button
-            onClick={() =>
-              (window.location.href = "https://spotifydesafio.luizchaves.dev")
-            }
-          >
-            Ir para desafio
-          </Button>
-          <Button onClick={() => setAlertModal(false)}>Fechar</Button>
-        </ModalFooter>
-      </Modal>
       <div id="homescreen">
         <h1>Spotify data by Luiz Chaves</h1>
         <p>A web app based on Spotify public API</p>
